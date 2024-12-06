2a78d00d9ed6044e5e90f82b24a2e40dfd01a404
Deiver Vasquez <estudiandovazmore@gmail.com>
2024-12-06 15:21:43 -0500

    solve bugs

--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -4,8 +4,6 @@ import { FC, useEffect, useState } from 'react';
 import { Accordion, AccordionItem, Button, Link } from '@nextui-org/react';
 import { FaBars } from 'react-icons/fa';
 import { useParams, useRouter } from 'next/navigation';
-import { hasPermission, hasPermissionStartsAt, PERMISSIONS } from '@/utils/hasPermissions';
-import useClientQuery from '@/stores/clients/useClientQuery';
 
 export interface SubRoute {
     title: string;
@@ -26,10 +24,7 @@ const Sidebar: FC<SidebarProps> = ({ menuItems }) => {
     const router = useRouter();
     const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
     const [screenHeight, setScreenHeight] = useState<number | null>(null);
-    const [filteredMenuItems, setFilteredMenuItems] = useState<MenuItem[]>([]);
 
-    // TODO: Quitar esto para usar la vista base
-    const { clients } = useClientQuery();
 
     useEffect(() => {
         setScreenHeight(window.screen.height);
@@ -43,39 +38,6 @@ const Sidebar: FC<SidebarProps> = ({ menuItems }) => {
         setIsSidebarOpen(false);
     };
 
-    useEffect(() => {
-        setFilteredMenuItems(filterMenuItems(menuItems));
-    }, [menuItems]);
-
-    const filterSubMenuItems = (subRoutes: SubRoute[]): SubRoute[] => {
-        return subRoutes.filter(subRoute => {
-            if (subRoute.title.toLowerCase().includes("nuevo") && !hasPermissionStartsAt("create")) {
-                return false;
-            }
-            return true;
-        });
-    };
-
-    const filterMenuItems = (items: MenuItem[]): MenuItem[] => {
-        return items.map(item => {
-            const filteredSubRoutes = filterSubMenuItems(item.subRoutes);
-            return {
-                ...item,
-                subRoutes: filteredSubRoutes
-            };
-        }).filter(item => {
-            if (item.title === 'Agenda') {
-                return hasPermission(PERMISSIONS.events.read);
-            } if (item.title === 'Clientes') {
-                return hasPermission(PERMISSIONS.clients.read);
-            } else if (item.title === 'Asesores') {
-                return hasPermission(PERMISSIONS.advisors.read);
-            } else if (item.title === 'Roles') {
-                return hasPermission(PERMISSIONS.roles.read);
-            }
-            return true;
-        });
-    };
 
     return (
         <div style={{ height: screenHeight ? `${screenHeight}px` : '100vh' }}>
@@ -91,7 +53,7 @@ const Sidebar: FC<SidebarProps> = ({ menuItems }) => {
 
             <div className={`fixed z-40 lg:static bg-gray-100 ${isSidebarOpen ? "w-full" : "w-64"} h-full p-4 transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0 transition-transform duration-300`}>
                 <Accordion>
-                    {filteredMenuItems.map((menuItem) => (
+                    {menuItems.map((menuItem) => (
                         <AccordionItem
                             key={menuItem.title}
                             aria-label={menuItem.title}
@@ -110,12 +72,7 @@ const Sidebar: FC<SidebarProps> = ({ menuItems }) => {
                                             className="p-2 text-black w-full h-full cursor-pointer"
                                             onClick={() => {
                                                 handleCloseSidebar();
-                                                // TODO: Quitar esto para usar la vista base
-                                                if (subRoute.title === "Directorio de clientes") {
-                                                    router.replace(`${subRoute.path}/${clients![0].ID ?? 6}`)
-                                                } else {
-                                                    router.replace(subRoute.path);
-                                                }
+                                                router.replace(subRoute.path);
                                             }}
                                         >
                                             {subRoute.title}
